5bfbfcdaee6a7fd9d7a0f48d425a56d583f01690
Timur Fatykhov <tfatykhov@gmail.com>
2016-02-23 14:18:07 -0500

    Update w.js

--- defaults/w.js
+++ defaults/w.js
@@ -1,7 +1,10 @@
 
 (function (){
-return {
+var wnr = {
         "w1":"43c25e61426951f409e41a75bb24ecc5",
         "w2":"cff4415f26e57d87c1cc7d56bb83c705"
       };
-});
+if (typeof define === 'function' && define.amd) define(wnr);
+else if (typeof module !== 'undefined') module.exports = wnr;
+else window.wnr = wnr;      
+}());
