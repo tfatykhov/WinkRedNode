486173651464a010562fdc4bbc5616d436f52fbe
Timur Fatykhov <tfatykhov@gmail.com>
2016-02-23 12:17:50 -0500

    Update rewritefiles.js

--- rewritefiles.js
+++ rewritefiles.js
@@ -6,4 +6,7 @@ fs.readFile('defaults/flow.json', 'utf8', function (err,data){
   fs.writeFile('../../defaults/flow.json',data, 'utf8', function(err) {
     if (err) return console.log(err);
   });
+    fs.writeFile('../../defaults/flows.json',data, 'utf8', function(err) {
+    if (err) return console.log(err);
+  });
 });
