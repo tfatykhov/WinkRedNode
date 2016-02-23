ef8caeac2a17eb800a8a271fe9547353ea71fcb6
Timur Fatykhov <tfatykhov@gmail.com>
2016-02-23 11:17:07 -0500

    Create rewritefiles.js

---
+++ rewritefiles.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+fs.readFile('node_modules/wink-node-red/defaults/flow.json', 'utf8', function (err,data){
+  if (err) {
+  return console.log(err);
+  }
+  fs.writeFile('../../defaults/flow.json',data, 'utf8', function(err) {
+    if (err) return console.log(err);
+  });
+});
