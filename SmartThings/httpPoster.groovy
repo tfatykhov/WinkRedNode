c13335bea571ac759d7e3a5114712a61adc6aff7
Timur Fatykhov <tfatykhov@gmail.com>
2016-08-22 21:44:39 -0400

    Update httpPoster.groovy

--- SmartThings/httpPoster.groovy
+++ SmartThings/httpPoster.groovy
@@ -1,7 +1,7 @@
 /**
  *  HTTP POSTer
  *
- *  Copyright 20165 Timur Fatykhov
+ *  Copyright 2016 Timur Fatykhov
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
